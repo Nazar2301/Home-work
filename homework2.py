0e4e295504c74ab9b104b24e832890de24a813c9
Nazar2301 <konstruktin@yandex.ru>
2024-07-29 11:20:34 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+first_name = "12"
+second_name = "1.5"
+well_name = "Python"
+well_time = 1.5/12
+print('Курс:', well_name,', всего задач:', first_name, ', затрачено часов:', second_name, ', среднее выполнение:', well_time)
