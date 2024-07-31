53eb068deedea2689e77ced84bd3aaf9a774048e
Nazar2301 <konstruktin@yandex.ru>
2024-07-31 13:51:51 +0300

    task4.py

--- task4.py
+++ task4.py
@@ -1,7 +1,7 @@
-print("4th program")
-ab = 13.42
-ba = 42.13
-a = print(int(ab-0.42)) and print(int((ba-42)*100))
-b = print(int(ba-0.13)) and print(int((ab-13)*100))
-print(a==a) and print(b==b)
+# 4th program
+a, b = 13.42, 42.13
+ai, bi = int(a), int(b)
+af, bf = int((a - ai) * 100), int((b - bi) * 100)
+print(ai == bf or bi == af)
+
 
