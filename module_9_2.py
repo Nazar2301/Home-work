35593bf57d3a224e45946f73b0ff1e618cfbba74
Nazar2301 <konstruktin@yandex.ru>
2024-10-10 10:41:44 +0300

    Add files via upload

---
+++ module_9_2.py
@@ -0,0 +1,15 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+
+first_result = [len(x) for x in first_strings if len(x) >= 5]
+
+
+second_result = [(y, x) for y in first_strings for x in second_strings if len(y) == len(x)]
+
+# Создание словаря, где ключ - строка, значение - длина строки, если длина строки четная
+third_result = {x: len(x) for x in first_strings + second_strings if len(x) % 2 == 0}
+
+print(first_result)
+print(second_result)
+print(third_result)
