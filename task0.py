11c907df5462666e66e43073cc7e11a04286ded9
Nazar2301 <konstruktin@yandex.ru>
2024-11-20 13:54:01 +0300

    Add files via upload

---
+++ task0.py
@@ -0,0 +1,18 @@
+# 1st program
+print("1st program")
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+number = 123.456
+shifted_number = number * 10
+first_digit_after_dot = int(shifted_number) % 10
+print(first_digit_after_dot)
